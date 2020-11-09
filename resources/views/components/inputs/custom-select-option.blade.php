a00a9550b264d6e17f7914021aaecc4989f39b20
Randall Wilk <wilk.randall@gmail.com>
2020-11-09 13:16:58 -0600

    change how option is selected

--- resources/views/components/inputs/custom-select-option.blade.php
+++ resources/views/components/inputs/custom-select-option.blade.php
@@ -1,14 +1,13 @@
 <li @unless ($disabled || $isGroup)
-        x-on:mouseenter="onMouseEnter($event.currentTarget.dataset.value)"
+        x-on:mouseenter="onMouseEnter({{ $optionValue() }})"
         x-on:mouseleave="selected = null; currentIndex = -1"
-        x-on:click="choose($event.currentTarget.dataset.value, $event)"
+        x-on:click="choose({{ $optionValue() }}, $event)"
     @endunless
 
     @unless ($isGroup)
         role="option"
         x-bind:class="{ 'focused': selected === {{ $optionValue() }}, 'selected': isChosen({{ $optionValue() }}) }"
         data-option="{{ json_encode($option) }}"
-        data-value="{{ $option['value'] }}"
         x-bind:aria-selected="JSON.stringify(isChosen({{ $optionValue() }}))"
     @endunless
 
