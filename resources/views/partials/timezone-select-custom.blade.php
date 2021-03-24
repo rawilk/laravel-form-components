4fe090abacf221771b9a6b82614126e417cec275
Mert Aşan <mertemreasan@gmail.com>
2021-03-25 02:16:42 +0300

    Update timezone-select-custom.blade.php

--- resources/views/partials/timezone-select-custom.blade.php
+++ resources/views/partials/timezone-select-custom.blade.php
@@ -1,11 +1,12 @@
-<x-custom-select :filterable="$filterable"
-                 :optional="$optional"
-                 :placeholder="$placeholder"
-                 :multiple="$multiple"
-                 :fixed-position="$fixedPosition"
-                 :max-width="$maxWidth"
-                 :options="$optionsForCustomSelect()"
-                 :container-class="$containerClass"
-                 :extra-attributes="$extraAttributes"
-                 {{ $attributes }}
+<x-dynamic-component :component="formComponentName('custom-select')"
+                     :filterable="$filterable"
+                     :optional="$optional"
+                     :placeholder="$placeholder"
+                     :multiple="$multiple"
+                     :fixed-position="$fixedPosition"
+                     :max-width="$maxWidth"
+                     :options="$optionsForCustomSelect()"
+                     :container-class="$containerClass"
+                     :extra-attributes="$extraAttributes"
+                     {{ $attributes }}
 />
