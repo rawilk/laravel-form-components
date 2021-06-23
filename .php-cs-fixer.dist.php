613729bdffa88d1e45aef1ef8a218dcb64622bbf
Randall Wilk <wilk.randall@gmail.com>
2021-06-23 15:01:49 -0500

    wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -32,7 +32,7 @@ return (new PhpCsFixer\Config)
         'phpdoc_var_without_name' => true,
         'class_attributes_separation' => [
             'elements' => [
-                'method',
+                'method' => 'one',
             ],
         ],
         'method_argument_space' => [
