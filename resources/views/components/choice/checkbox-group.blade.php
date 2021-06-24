fac61fc0c1a34c20b6c8bb295ab8b8c05de0b15a
Randall Wilk <wilk.randall@gmail.com>
2021-06-24 10:29:18 -0500

    Change merge to class

--- resources/views/components/choice/checkbox-group.blade.php
+++ resources/views/components/choice/checkbox-group.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->merge(['class' => $stacked ? 'space-y-4' : 'form-checkbox-group grid gap-4 items-start']) }}
+<div {{ $attributes->class($stacked ? 'space-y-4' : 'form-checkbox-group grid gap-4 items-start') }}
      @unless ($stacked) style="--fc-checkbox-grid-cols: {{ $gridCols }};" @endunless
 >
     {{ $slot }}
