3bbfa332edda384b8c3c8f06b5ab0d7c4b278540
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2021-08-12 17:31:45 +0430

    Add Farsi/Persian Languages.

---
+++ resources/lang/fa/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'filepond_instructions' => '<span class="filepond--label-action">بارگذاری فایل</span> یار بکشید و رها کنید',
+    'file_upload_label' => 'انتخاب فایل',
+    'file_upload_processing' => 'درحال پردازش...',
+    'custom_select_filter_placeholder' => 'جستجو...',
+    'custom_select_clear_button' => 'خالی کردن انتخاب شده ها',
+    'custom_select_placeholder' => 'انتخاب یک گزینه',
+    'custom_select_empty_text' => 'گزینه قابل انتخابی وجود ندارد...',
+    'date_picker_placeholder' => 'Y-m-d',
+    'timezone_select_placeholder' => 'انتخاب زمان محلی',
+    'switch_button_label' => 'روشن کن',
+    'optional' => 'اختیاری',
+    'date_picker_toggle_icon_title' => 'انتخاب تاریخ',
+    'date_picker_clear_button' => 'پا کردن انتخاب',
+    'password_show_toggle_title' => 'نمایش دادن',
+    'password_hide_toggle_title' => 'مخفی کردن',
+];
