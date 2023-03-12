ae114ca92846f2d8d406f1b8b26ce6c1b05658e8
Randall Wilk <wilk.randall@gmail.com>
2023-03-12 09:42:39 -0500

    Update textarea tests

--- tests/Components/Inputs/TextareaTest.php
+++ tests/Components/Inputs/TextareaTest.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Route;
 use function Pest\Laravel\get;
 use Sinnbeck\DomAssertions\Asserts\AssertElement;
 
+beforeEach(function () {
+    config()->set('form-components.defaults.textarea', [
+        'rows' => 3,
+        'auto_resize' => true,
+    ]);
+});
+
 it('can be rendered', function () {
     Route::get('/test', fn () => Blade::render('<x-textarea name="about" />'));
 
@@ -15,7 +22,8 @@ it('can be rendered', function () {
             $textarea->is('textarea')
                 ->has('name', 'about')
                 ->has('id', 'about')
-                ->has('class', 'form-input');
+                ->has('class', 'form-text')
+                ->has('x-textarea-resize');
         });
 });
 
@@ -27,7 +35,7 @@ it('allows custom attributes', function () {
             $textarea->is('textarea')
                 ->has('name', 'about')
                 ->has('id', 'aboutMe')
-                ->has('class', 'form-input p-4')
+                ->has('class', 'form-text p-4')
                 ->has('rows', '10')
                 ->has('cols', '20');
         });
@@ -63,5 +71,41 @@ it('accepts a container class', function () {
                 ->contains('textarea', [
                     'name' => 'about',
                 ]);
+        });
+});
+
+test('container class can be applied by default', function () {
+    config()->set('form-components.defaults.input.container_class', 'default-container');
+
+    Route::get('/test', fn () => Blade::render('<x-textarea name="about" />'));
+
+    get('/test')
+        ->assertElementExists('.form-text-container', function (AssertElement $div) {
+            $div->has('class', 'default-container')
+                ->contains('textarea', [
+                    'name' => 'about',
+                ]);
+        });
+});
+
+test('rows can be set by default', function () {
+    config()->set('form-components.defaults.textarea.rows', 10);
+
+    Route::get('/test', fn () => Blade::render('<x-textarea name="about" />'));
+
+    get('/test')
+        ->assertElementExists('textarea', function (AssertElement $textarea) {
+            $textarea->has('rows', '10');
+        });
+});
+
+test('auto resize can be disabled by default', function () {
+    config()->set('form-components.defaults.textarea.auto_resize', false);
+
+    Route::get('/test', fn () => Blade::render('<x-textarea name="about" />'));
+
+    get('/test')
+        ->assertElementExists('textarea', function (AssertElement $textarea) {
+            $textarea->doesntHave('x-textarea-resize');
         });
 });
