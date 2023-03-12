b35a837aacde7c6fdfe41ad247b1efcc8b7f90a3
Randall Wilk <wilk.randall@gmail.com>
2023-03-12 09:29:30 -0500

    Update input tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,6 +4,12 @@ use Rawilk\FormComponents\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
 
+uses()
+    ->beforeEach(function () {
+        config()->set('form-components.defaults.global.show_errors', true);
+    })
+    ->in(__DIR__ . '/Components');
+
 // Helpers
 
 function flashOld(array $input): void
