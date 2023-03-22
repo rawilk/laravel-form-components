4895b08483291ae135acb91c18dfeb4940ba406e
Randall Wilk <wilk.randall@gmail.com>
2023-03-22 10:45:57 -0500

    Prevent textarea-resize from firing on hidden textareas

--- resources/js/directives/textarea-resize.js
+++ resources/js/directives/textarea-resize.js
@@ -1,12 +1,27 @@
 const resize = el => {
+    if (! el.style.minHeight) {
+        el.style.minHeight = `${el.scrollHeight}px`;
+    }
+
     el.style.height = 'auto';
     el.style.height = `${el.scrollHeight}px`;
 };
 
+const isHidden = el => {
+    if (el.style.display === 'none') {
+        return true;
+    }
+
+    return ! el.offsetParent;
+};
+
 export default Alpine => {
     Alpine.directive('textarea-resize', (el, {}, { cleanup }) => {
-        el.style.minHeight = `${el.scrollHeight}px`;
-        resize(el);
+        // We should not attempt to resize the textarea when it is not visible,
+        // otherwise the height will be set to 0.
+        if (! isHidden(el)) {
+            resize(el);
+        }
 
         const inputHandler = () => resize(el);
 
