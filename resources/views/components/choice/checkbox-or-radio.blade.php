0e9be2a326bded214689200b5d720800c598aea9
Randall Wilk <wilk.randall@gmail.com>
2023-03-23 07:41:10 -0500

    Always render value if it is set

--- resources/views/components/choice/checkbox-or-radio.blade.php
+++ resources/views/components/choice/checkbox-or-radio.blade.php
@@ -17,7 +17,7 @@
             @endif
             {!! $ariaDescribedBy() !!}
             {{ $extraAttributes ?? '' }}
-            @if ($value && ! $hasBoundModel()) value="{{ $value }}" @endif
+            @if ($value) value="{{ $value }}" @endif
             @checked($checked && ! $hasBoundModel())
         />
     </div>
